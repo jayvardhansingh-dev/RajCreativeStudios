7d8e84ce0b864388205f9205861bfc2b2b491d93
Jayvardhan Singh <jayvardhanknowledge@gmail.com>
2026-06-02 10:09:29 +0530

    Raj Creative Studios Website

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.getElementById("bookBtn").addEventListener("click", function(){
+    alert("Thank you for choosing Raj Creative Studios!");
+});
+
+document.getElementById("contactBtn").addEventListener("click", function(){
+    window.location.href = "tel:+911234567890";
+});
