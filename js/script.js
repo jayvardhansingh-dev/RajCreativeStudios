2d9ce2cbe8cac338690b049f309400a5a45e5075
Jayvardhan Singh <jayvardhanknowledge@gmail.com>
2026-06-02 22:59:14 +0530

    Added portfolio lightbox

--- js/script.js
+++ js/script.js
@@ -59,3 +59,14 @@ function shareWebsite() {
         alert("Website link copied!");
     }
 }
+function openLightbox(imageSrc){
+
+    document.getElementById("lightbox").style.display = "flex";
+
+    document.getElementById("lightbox-img").src = imageSrc;
+}
+
+function closeLightbox(){
+
+    document.getElementById("lightbox").style.display = "none";
+}
