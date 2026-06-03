d9d117c86ceb9ad2eedb8cd7151d85575d140c70
Jayvardhan Singh <jayvardhanknowledge@gmail.com>
2026-06-03 11:17:05 +0530

    Update Website

--- js/script.js
+++ js/script.js
@@ -77,3 +77,15 @@ document.getElementById("lightbox").addEventListener("click", function(e){
     }
 
 });
+
+function openLightbox(imageSrc){
+
+    document.getElementById("lightbox").style.display = "flex";
+
+    document.getElementById("lightbox-img").src = imageSrc;
+}
+
+function closeLightbox(){
+
+    document.getElementById("lightbox").style.display = "none";
+}
